e20aef60f0b9dbb48c0741c1ee29e2dadb28e886
Royce Pipkins <rpipkins@thinkliquidity.com>
2021-12-22 09:32:15 -0600

    Refactor the ESPHome atom config files.

---
+++ esp_atom_remote_switch/flash_all_atoms.bat
@@ -0,0 +1,60 @@
+esphome -s deviceName atom-01 run esp_atom_common.yaml
+pause
+esphome -s deviceName atom-02 run esp_atom_common.yaml
+pause
+esphome -s deviceName atom-03 run esp_atom_common.yaml
+pause
+esphome -s deviceName atom-04 run esp_atom_common.yaml
+pause
+esphome -s deviceName atom-05 run esp_atom_common.yaml
+pause
+esphome -s deviceName atom-06 run esp_atom_common.yaml
+pause
+esphome -s deviceName atom-07 run esp_atom_common.yaml
+pause
+esphome -s deviceName atom-08 run esp_atom_common.yaml
+pause
+esphome -s deviceName atom-09 run esp_atom_common.yaml
+pause
+esphome -s deviceName atom-10 run esp_atom_common.yaml
+pause
+esphome -s deviceName atom-11 run esp_atom_common.yaml
+pause
+esphome -s deviceName atom-12 run esp_atom_common.yaml
+pause
+esphome -s deviceName atom-13 run esp_atom_common.yaml
+pause
+esphome -s deviceName atom-14 run esp_atom_common.yaml
+pause
+esphome -s deviceName atom-15 run esp_atom_common.yaml
+pause
+esphome -s deviceName atom-16 run esp_atom_common.yaml
+pause
+esphome -s deviceName atom-17 run esp_atom_common.yaml
+pause
+esphome -s deviceName atom-18 run esp_atom_common.yaml
+pause
+esphome -s deviceName atom-19 run esp_atom_common.yaml
+pause
+esphome -s deviceName atom-20 run esp_atom_common.yaml
+pause
+esphome -s deviceName atom-21 run esp_atom_common.yaml
+pause
+esphome -s deviceName atom-22 run esp_atom_common.yaml
+pause
+esphome -s deviceName atom-23 run esp_atom_common.yaml
+pause
+esphome -s deviceName atom-24 run esp_atom_common.yaml
+pause
+esphome -s deviceName atom-25 run esp_atom_common.yaml
+pause
+esphome -s deviceName atom-26 run esp_atom_common.yaml
+pause
+esphome -s deviceName atom-27 run esp_atom_common.yaml
+pause
+esphome -s deviceName atom-28 run esp_atom_common.yaml
+pause
+esphome -s deviceName atom-29 run esp_atom_common.yaml
+pause
+esphome -s deviceName atom-30 run esp_atom_common.yaml
+pause
